b1875b83079583778e4ea4915870977159f64ffc
0x00f <ncodedeco@gmail.com>
2018-03-22 12:26:07 -0500

    Fixed: Layouts on activities.
    1. Can't get another activity to start from login page to home page..
    2. Need to fix VenuesAndArtists activity so it displays collapsable toolbar with image that then leads to description
    3. Need to adjust FAB's to ImageButtons in the Framelayout on the Homepage.

--- app/src/main/java/edu/augsburg/univent/VenuesAndArtistsActivity.java
+++ app/src/main/java/edu/augsburg/univent/VenuesAndArtistsActivity.java
@@ -1,7 +1,7 @@
 package edu.augsburg.univent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class VenuesAndArtistsActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class VenuesAndArtistsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venues_and_artists);
+        android.support.v7.widget.Toolbar myToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
