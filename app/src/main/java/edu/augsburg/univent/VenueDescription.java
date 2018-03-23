2c306202385557adcaab5bfecc1c38fc31c0c737
0x00f <ncodedeco@gmail.com>
2018-03-23 16:14:21 -0500

    Added:  A favorites star. Removed:  VenuesAndArtistsActivity.java and all related files Working on:  Finish designing toolbar for activities      HELP CANNOT FIGURE OUT  Create images for Artist and Venue description  Linking Login to Homepage with intents  Designing Settings tab and get sliding animation to work

--- app/src/main/java/edu/augsburg/univent/VenueDescription.java
+++ app/src/main/java/edu/augsburg/univent/VenueDescription.java
@@ -1,8 +1,12 @@
 package edu.augsburg.univent;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class VenueDescription extends AppCompatActivity {
 
@@ -10,9 +14,26 @@ public class VenueDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_description);
-        android.support.v7.widget.Toolbar myToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
+        //Add toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_venues_description, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_settings):
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
